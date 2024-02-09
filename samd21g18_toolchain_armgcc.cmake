452d5f27f5681e2b5e47c87c24ee4ea33e612333
Victor Hogeweij <vo.hogeweij@student.han.nl>
2024-02-09 08:13:55 +0100

    initial commit

---
+++ samd21g18_toolchain_armgcc.cmake
@@ -0,0 +1,45 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+if(${CMAKE_VERSION} VERSION_LESS "3.16.0")
+    message(WARNING "Current CMake version is ${CMAKE_VERSION}. This toolchain requires CMake 3.16 or greater")
+
+endif()
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+
+
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP arm-none-eabi-objdump)
+set(SIZE arm-none-eabi-size)
+set(MCPU cortex-m0plus)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+add_definitions(-D__SAMD21G18A__
+                -fno-common 
+                -g3 
+                -gdwarf-4 
+                -Wall 
+                -fmessage-length=0 
+                -fno-builtin 
+                -ffunction-sections 
+                -fdata-sections 
+                -fmerge-constants
+                -mapcs)
+
+set(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/linker_script.ld)
+add_link_options(-T ${LINKER_SCRIPT}
+                -mthumb
+                -mcpu=${MCPU}
+                -specs=nano.specs 
+                -Wl,--gc-sections
+                --specs=nosys.specs
+                -Wl,--print-memory-usage
+                -lm
+                -Wl,-Map=${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map)
+
+include_directories("cmsis-header-sam/samd21a/gcc/"
+					"cmsis-header-sam/samd21a/gcc/gcc/"
+					"CMSIS_5/CMSIS/Core/Include/"
+					"cmsis-header-sam/samd21a/include")
+
+add_library(CMSIS INTERFACE "gcc/system_samd21.c" "gcc/gcc/startup_samd21.c")
